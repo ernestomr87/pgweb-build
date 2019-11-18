ca1c23d13a4270f50c6ff938c21002fbe4b61492
Ernesto M Rodriguez <ernestomr87@gmail.com>
2019-11-18 11:26:28 -0500

    Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.773d62ffe38eb8db34a8a401047181a7.js"
+  "/precache-manifest.1e106d3453bd6be244ad447aade9b16c.js"
 );
 
 self.addEventListener('message', (event) => {
