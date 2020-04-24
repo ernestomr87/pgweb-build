02a2175fccdcbf4b6c4dac56044be64266efd5fb
leoperezortiz <leoperezortiz@gmail.com>
2020-04-24 02:24:40 +0200

    update

--- static/js/40.fb0bf9f1.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[40,41],{1429:function(e,a,t){"use strict";t.r(a);var n=t(0),d=t.n(n),o=t(70),c=t(1430),r=t.n(c);a.default=Object(o.c)(function(e){var a=e.intl.messages;return d.a.createElement(r.a,{type:"404",desc:a.default.notFound,backText:a.default.goHome,redirect:e.dashboard?"/dashboard":"/"})})}}]);
-//# sourceMappingURL=40.fb0bf9f1.chunk.js.map
